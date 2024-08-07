a97ef9b14ccb06828619d797be80ef43c0fe7653
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-08-07 00:07:21 -0600

    Arreglo Navbar

--- chat/src/Views/Home/Home.js
+++ chat/src/Views/Home/Home.js
@@ -1,4 +1,9 @@
 import React from "react";
+import Icon from '@mdi/react';
+import { mdiMessageText } from '@mdi/js';
+import { mdiContacts } from '@mdi/js';
+import { mdiAccountGroup } from '@mdi/js';
+import { mdiCog } from '@mdi/js';
 import './Home.css';
 
 function Home() {
@@ -6,7 +11,28 @@ function Home() {
         <div className="Home">
             <div className="Navbar"></div>
             <div className="Chat">
-                <div className="ChatNavbar"></div>
+                <div className="ChatNavbar">
+                    <div className="ChatIcon" style={{ alignItems: 'center', justifyContent: 'center' }}>
+                        <button className="iconButton">
+                            <Icon path={mdiMessageText} size={1.2} color="#7B8990" />
+                        </button>
+                    </div>
+                    <div className="ContactsIcon">
+                        <button className="iconButton">
+                            <Icon path={mdiContacts} size={1.2} color="#7B8990" />
+                        </button>
+                    </div>
+                    <div className="GroupsIcon">
+                        <button className="iconButton">
+                            <Icon path={mdiAccountGroup} size={1.2} color="#7B8990" />
+                        </button>
+                    </div>
+                    <div className="SettingsIcon">
+                        <button className="iconButton">
+                            <Icon path={mdiCog} size={1.2} color="#7B8990" />
+                        </button>
+                    </div>
+                </div>
                 <div className="ChatMessages">
                     <h4>Chats</h4>
                 </div>
