17af6565d20f2532548b6867d6944b0008b6be9f
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-08-06 20:17:34 -0600

    Diseño

---
+++ chat/src/Views/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="Home">
+            <div className="Navbar"></div>
+            <div className="Chat">
+                <div className="ChatNavbar"></div>
+                <div className="ChatMessages"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
