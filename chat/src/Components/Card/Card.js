024705099c8b3312bab24633006188704d94579a
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-08-11 18:49:04 -0600

    Diseño información de contacto

--- chat/src/Components/Card/Card.js
+++ chat/src/Components/Card/Card.js
@@ -1,18 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Icon from '@mdi/react';
+import { mdiAccountEye } from '@mdi/js';
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+        padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+        padding: theme.spacing(1),
+    },
+}));
 
 function ChatCard(props) {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <Card >
             <CardContent>
-                <Typography variant="h6" component="div">
-                    {props.name}
-                </Typography>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography variant="h6" component="div">
+                        {props.name}
+                    </Typography>
+                    <button onClick={handleClickOpen} style={{ backgroundColor: "transparent", border: "none" }} >
+                        <Icon path={mdiAccountEye} size={1.2} color="#7B8990" />
+                    </button>
+                </div>
                 <Typography variant="subtitle2" color="text.secondary">
                     {props.status}
                 </Typography>
+
+                <BootstrapDialog
+                    onClose={handleClose}
+                    aria-labelledby="customized-dialog-title"
+                    open={open}
+                >
+                    <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+                        {props.name}
+                    </DialogTitle>
+                    <IconButton
+                        aria-label="close"
+                        onClick={handleClose}
+                        sx={{
+                            position: 'absolute',
+                            right: 8,
+                            top: 8,
+                            color: (theme) => theme.palette.grey[500],
+                        }}
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                    <DialogContent dividers>
+                        <Typography gutterBottom>
+                            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                            consectetur ac, vestibulum at eros.
+                        </Typography>
+                        <Typography gutterBottom>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+                            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                        </Typography>
+                        <Typography gutterBottom>
+                            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
+                            magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
+                            ullamcorper nulla non metus auctor fringilla.
+                        </Typography>
+                    </DialogContent>
+                </BootstrapDialog>
             </CardContent>
         </Card>
     );
